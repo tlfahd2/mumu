b9ec3ea1622e193272f99c1f719563c7c57b62bf
tlfahd2 <tlfahd2@naver.com>
2023-11-17 14:28:33 +0900

    accounts중

--- back/final/urls.py
+++ back/final/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/movies/', include('movies.urls')),
     path('api/v1/community', include('communities.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
 ]
